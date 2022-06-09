628e1e90e462c51304d186327665369f202f487c
Kaz Gofron <kgofron@bnl.gov>
2022-06-09 18:33:23 -0400

    start ioc requires URI cpr library

--- iocs/tpx3IOC/iocBoot/iocTimePix/st.cmd
+++ iocs/tpx3IOC/iocBoot/iocTimePix/st.cmd
@@ -1,51 +1,5 @@
-#!../../bin/linux-x86_64/tpx3App
+#!/bin/bash
 
-< envPaths
+export LD_LIBRARY_PATH=../../../../tpx3Support/cprSrc/cpr/build/lib:$LD_LIBRARY_PATH
 
-< unique.cmd
-
-
-epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
-errlogInit(20000)
-
-#epicsThreadSleep(20)
-dbLoadDatabase("$(ADTIMEPIX)/iocs/tpx3IOC/dbd/tpx3App.dbd")
-tpx3App_registerRecordDeviceDriver(pdbbase)
-
-
-
-
-epicsEnvSet("SERVER_URL", "http://localhost:8080")
-epicsEnvSet("PREFIX", "TPX3-TEST:")
-
-
-# If searching for device by product ID put "" or empty string for serial number
-ADTimePixConfig("$(PORT)", "$(SERVER_URL)", 0, 0, 0, 0)
-epicsThreadSleep(2)
-
-asynSetTraceIOMask($(PORT), 0, 2)
-#asynSetTraceMask($(PORT),0,0xff)
-
-dbLoadRecords("$(ADCORE)/db/ADBase.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADTIMEPIX)/db/ADTimePix.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-
-#
-# Create a standard arrays plugin, set it to get data from Driver.
-#int NDStdArraysConfigure(const char *portName, int queueSize, int blockingCallbacks, const char *NDArrayPort, int NDArrayAddr, int maxBuffers, size_t maxMemory,
-#                          int priority, int stackSize, int maxThreads)
-NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
-dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,NDARRAY_PORT=$(PORT),TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=20000000")
-
-#
-# Load all other plugins using commonPlugins.cmd
-< $(ADCORE)/iocBoot/commonPlugins.cmd
-#
-
-set_requestfile_path("$(ADTIMEPIX)/tpx3App/Db")
-
-#asynSetTraceMask($(PORT),0,0x09)
-#asynSetTraceMask($(PORT),0,0x11)
-iocInit()
-
-# save things every thirty seconds
-create_monitor_set("auto_settings.req", 30, "P=$(PREFIX)")
+../../bin/linux-x86_64/tpx3App st_base.cmd
