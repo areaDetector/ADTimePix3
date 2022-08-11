9bf560381ef0bc7cf656a5c07677b3590d31d9b1
Kaz Gofron <kgofron@bnl.gov>
2022-08-11 14:03:06 -0400

    start ioc on linux

--- iocs/tpx3IOC/iocBoot/iocTimePix/startEPICS.sh
+++ iocs/tpx3IOC/iocBoot/iocTimePix/startEPICS.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export EPICS_CA_MAX_ARRAY_BYTES=10000000
-../../bin/linux-x86_64/DRIVERNAMELOWERSHORTApp st.cmd
+export LD_LIBRARY_PATH=../../../../tpx3Support/cprSrc/cpr/build/lib:$LD_LIBRARY_PATH
+../../bin/linux-x86_64/tpx3App st_base.cmd
