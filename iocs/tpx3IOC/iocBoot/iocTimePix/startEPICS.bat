e18b26193b0781b0e16d8c66f52749c09daab0a4
Kaz Gofron <kgofron@bnl.gov>
2022-08-11 14:02:34 -0400

    start ioc on windows

--- iocs/tpx3IOC/iocBoot/iocTimePix/startEPICS.bat
+++ iocs/tpx3IOC/iocBoot/iocTimePix/startEPICS.bat
@@ -1,4 +1,4 @@
-rem Starts the ADDRIVERNAMESTANDARD on windows
+rem Starts the ADTimePix on windows
 
 set EPICS_CA_MAX_ARRAY_BYTES=10000000
-../../bin/windows-x64-static/DRIVERNAMELOWERSHORTApp st.cmd
+../../bin/windows-x64-statictpx3App st_base.cmd
