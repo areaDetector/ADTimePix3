c92aa5a0660bbd43005c4beb84bde0ccc5a2bc88
K. Gofron <kgofron@comcast.net>
2026-04-17 17:02:20 -0400

    chore(iocBoot): remove unused startEPICS.bat and startEPICS.sh

--- iocs/tpx3IOC/iocBoot/iocTimePix/startEPICS.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export EPICS_CA_MAX_ARRAY_BYTES=10000000
-export LD_LIBRARY_PATH=../../../../tpx3Support/cprSrc/cpr/build/lib:$LD_LIBRARY_PATH
-../../bin/linux-x86_64/tpx3App st_base.cmd
